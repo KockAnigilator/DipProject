478f3b9924c54be59a3a458f96c82ee3aa8e3380
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-03-05 19:54:59 +0300

    Класс для базы данных

---
+++ ParserDB/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserDB
+{
+    public class Car
+    {
+        public int Id { get; set; } // Уникальный идентификатор
+        public string Name { get; set; } // Название автомобиля
+        public string Price { get; set; } // Цена
+        public string Year { get; set; } // Год выпуска
+        public string Mileage { get; set; } // Пробег
+        public string Link { get; set; } // Ссылка на объявление
+    }
+}
