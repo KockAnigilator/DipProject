4c0aa9ef159c7b3f2dfcb86cf293bedc0024c225
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-03-07 20:41:46 +0300

    Репозиторий для user

---
+++ ParserDB/Репозитории/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserDB.Репозитории
+{
+    class UserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Добавление пользователя
+        public void AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        // Получение пользователя по ID
+        public User GetUserById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        // Получение всех пользователей
+        public List<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
