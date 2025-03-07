faee6336fe9bcb0fbf462125632077e16a0cc99f
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-03-07 20:40:42 +0300

    WIP репозитории для каждого базого класса

---
+++ ParserDB/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserDB
+{
+    class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=CarMarketDB") { }
+
+        // DbSet для каждый таблицы
+        // DbSet для каждой таблицы
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Favourite> Favourites { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Настройка связи многие-ко-многим между User и Car через Favourite
+            modelBuilder.Entity<Favourite>()
+                .HasKey(f => new { f.UserId, f.CarId });
+
+            modelBuilder.Entity<Favourite>()
+                .HasRequired(f => f.user)
+                .WithMany(u => u.Favourites)
+                .HasForeignKey(f => f.UserId);
+
+            modelBuilder.Entity<Favourite>()
+                .HasRequired(f => f.car)
+                .WithMany()
+                .HasForeignKey(f => f.CarId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ ParserDB/Репозитории/CarRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserDB.Репозитории
+{
+    class CarRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CarRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Добавление автомобиля
+        public void AddCar(Car car)
+        {
+            _context.Cars.Add(car);
+            _context.SaveChanges();
+        }
+
+        // Получение всех автомобилей
+        public List<Car> GetAllCars()
+        {
+            return _context.Cars.ToList();
+        }
+
+        // Получение автомобиля по ID
+        public Car GetCarById(int id)
+        {
+            return _context.Cars.Find(id);
+        }
+
+        // Удаление автомобиля
+        public void DeleteCar(int id)
+        {
+            var car = _context.Cars.Find(id);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
