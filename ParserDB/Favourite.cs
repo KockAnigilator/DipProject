43a6248cc717fcadd55bf3c62e2f31a8ac0b5b76
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-03-07 20:33:56 +0300

    WIP

---
+++ ParserDB/Favourite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserDB
+{
+    class Favourite
+    {
+        public int UserId { get; set; } //Индефикатор пользлователя
+        public int CarId { get; set; } // Индефикатор автомобиля
+
+
+
+        //Вспомагательные свойства для навигации
+        public virtual User user { get; set; }
+        public virtual Car car { get; set; }
+    }
+}
