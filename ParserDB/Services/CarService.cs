4553c40d1d586eeb8e54e91be3b47fcdd58b0810
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-03-07 20:46:52 +0300

    + Сервисы для классов User и Car

---
+++ ParserDB/Services/CarService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParserDB.Репозитории;
+
+namespace ParserDB.Services
+{
+    class CarService
+    {
+        private readonly CarRepository _carRepository;
+
+        public CarService(CarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        // Получение всех автомобилей
+        public List<Car> GetAllCars()
+        {
+            return _carRepository.GetAllCars();
+        }
+
+        // Добавление автомобиля
+        public void AddCar(Car car)
+        {
+            _carRepository.AddCar(car);
+        }
+
+        // Удаление автомобиля
+        public void DeleteCar(int id)
+        {
+            _carRepository.DeleteCar(id);
+        }
+    }
+}
---
+++ ParserDB/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParserDB.Репозитории;
+
+namespace ParserDB.Services
+{
+    class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // Добавление пользователя
+        public void AddUser(User user)
+        {
+            _userRepository.AddUser(user);
+        }
+
+        // Получение пользователя по ID
+        public User GetUserById(int id)
+        {
+            return _userRepository.GetUserById(id);
+        }
+    }
+}
