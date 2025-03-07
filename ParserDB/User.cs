dc6c8579fe7ba825a0dceddfc314635f1b349daa
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-03-07 20:30:57 +0300

    WIP

---
+++ ParserDB/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserDB
+{
+    class User
+    {
+        private int Id { get; set; } // Айди
+        private int Name { get; set; } // Имя пользователя
+        private int ProfilePic { get; set; } // Путь к изображению
+
+        // Навигационное свойство для избранных автомобилей
+        public virtual ICollection<Favourite> Favourites { get; set; }
+    }
+}
